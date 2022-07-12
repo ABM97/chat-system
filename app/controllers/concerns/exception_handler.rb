8f469d329b4e35dd04c4627ede3a4865e78b01df
Abdallah Mahmoud <abdallah.abdelkareem.ext@orange.com>
2022-07-12 14:29:18 +0200

    add basic exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,23 +1,30 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
-  class AuthenticationError < StandardError; end
-
-  class MissingToken < StandardError; end
-
-  class InvalidToken < StandardError; end
+  ERRORS = {
+    'ActiveRecord::RecordNotFound' => 'Errors::NotFound',
+    'ActiveRecord::RecordInvalid' => 'Errors::UnprocessableEntity',
+  }
 
   included do
-    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: { message: e.message }, status: not_found
-    end
+    rescue_from(StandardError, with: lambda { |e| handle_error(e) })
   end
 
   private
 
-  def four_twenty_two(e)
-    render json: { message: e.message }, status: unprocessable_entity
+  def handle_error(e)
+    mapped = map_error(e)
+    mapped ||= Errors::StandardError.new
+    render_error(mapped)
+  end
+
+  def map_error(e)
+    error_klass = e.class.name
+    e if ERRORS.values.include?(error_klass)
+  end
+
+  def render_error(error)
+    render json: ErrorSerializer.new(error), status: error.status
   end
 
 end
---
+++ lib/errors/standard_error.rb
@@ -0,0 +1,46 @@
+module Errors
+  class StandardError < ::StandardError
+    def initialize(title: nil, detail: nil, status: nil)
+      @title = title || "Something went wrong"
+      @detail = detail || "We encountered unexpected error, but our developers had been already notified about it"
+      @status = status || 500
+    end
+
+    def to_h
+      {
+        status: status,
+        title: title,
+        detail: detail
+      }
+    end
+
+    def serializable_hash
+      to_h
+    end
+
+    def to_s
+      to_h.to_s
+    end
+
+    attr_reader :title, :detail, :status
+  end
+
+  class NotFound < Errors::StandardError
+    def initialize
+      super(
+        title: "Record not Found",
+        status: 404,
+        detail: "We could not find the object you were looking for."
+      )
+    end
+  end
+
+  class UnprocessableEntity < Errors::StandardError
+    def initialize
+      super(
+        title: "Unprocessable Entity",
+        status: 422
+      )
+    end
+  end
+end
